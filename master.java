4886658ad6d2e06970fc38cf740e26fe32d3929c
rytlu <kamil.rytel.kuc96@gmail.com>
2024-03-03 12:31:57 +0100

    chore: init project

--- master.java
+++ master.java
@@ -1,8 +1,12 @@
 public class master {
     public static void main(String[]args){
-        System.out.println("Hello, World!");
+        System.out.println("Hello World!");
+        // Inicjalizacja i deklaracja zmiennych
+        String myName = "Kamil Rytel-Kuc";
+        int albumNumber = 136407;
 
-
+        //
+        System.out.println("Data: " + myName + " " + albumNumber);
 
 
 
